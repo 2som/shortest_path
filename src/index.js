45cca5f2452fe40cb18b722ebfa26b0b223dbe5c
maks <maksswierczynski@gmail.com>
2020-07-27 18:37:42 +0200

    refactor main function into class

--- src/index.js
+++ src/index.js
@@ -1,41 +1,64 @@
-import * as ui from './user_interface.js'
-import {Board} from './board.js'
+import Board from './board.js';
+import Visualizer from './visualizer.js';
+import Solver from './solver.js';
+
+const CANVAS = document.getElementById(('canvas'));
+const DIMENSIONS = 20;
 
 const SETTINGS = {
-    canvas: document.getElementById('canvas'),
-    dimensions: 20,
+    canvas: CANVAS,
+    dimensions: DIMENSIONS,
+    rect_width: Math.floor(CANVAS.width / DIMENSIONS),
+    rect_height: Math.floor(CANVAS.height / DIMENSIONS)
 }
-SETTINGS.rect_width = Math.floor(SETTINGS.canvas.width / SETTINGS.dimensions)
-SETTINGS.rect_height = Math.floor(SETTINGS.canvas.height / SETTINGS.dimensions)
 
-function main(){
-    let rectangles = ui.create_rectangles(SETTINGS) //create empty canvas board
-    ui.draw(rectangles, SETTINGS)
 
-    SETTINGS.canvas.addEventListener('click', (e) => { //if canvas element is clicked, place an obstacle
-        if (SETTINGS.canvas.classList.contains('disabled')) {return}
-        ui.place_obstacle(e, rectangles, SETTINGS )
-        ui.draw(rectangles, SETTINGS);
-    })  
+class App {
+    constructor(settings) {
+        this.settings = settings;
+        this.disabled = false;
+        this.board;
+        this.visualizer;
+        this.solver;
+        this.clear_btn = document.getElementById('clear');
+        this.solve_btn = document.getElementById('solve');
+        this.create()
+        this.add_click_handlers()
+    }
+    create(){
+        this.board = new Board(this.settings);
+        this.visualizer = new Visualizer(this.settings, this.board.get_board());
+        this.visualizer.draw_board();
+    }
+
+    add_click_handlers(){
+        this.settings.canvas.addEventListener('click', (e) => this.click_board(e)) ;
+        this.clear_btn.addEventListener('click', (e) => this.clear_board(e));
+        this.solve_btn.addEventListener('click', (e) => this.solve_board(e));
+    }
+
+    click_board(e){
+        this.disabled ? null : this.visualizer.apply_obstacle(e);
+    }
+
+    clear_board(e){
+        this.board = new Board(this.settings);
+        this.visualizer = new Visualizer(this.settings, this.board.get_board());
+        this.visualizer.draw_board();
+        this.disabled = false;
+    }
+
+    async solve_board(e){
+        this.solver = new Solver(this.settings, this.board.get_board());
+        this.disabled = true;
+        this.solver.findPath() ? this._visualize(): alert("no valid path");
+    }
+
+    async _visualize(){
+        await this.visualizer.visualize_solving(this.solver.get_visited_rectangles())
+        await this.visualizer.visualize_path(this.solver.get_path())
+    }
     
-    document.getElementById('clear').addEventListener('click', (e) =>{ //reset the canvas board
-        rectangles = ui.create_rectangles(SETTINGS)
-        ui.draw(rectangles, SETTINGS)
-        SETTINGS.canvas.classList.remove('disabled');
-       
-    })
-    
-    document.getElementById('solve').addEventListener('click', (e) => {
-        const board = new Board(rectangles, SETTINGS.dimensions)
-        board.solve()
-        SETTINGS.canvas.classList.add('disabled');
-        if(board.is_solved){
-            ui.draw(board.graphic_representation, SETTINGS)
-        }
-        else{
-            alert('There is no valid path')
-            document.getElementById('clear').click();
-        }
-    })
 }
-main()
+
+const app = new App(SETTINGS);
